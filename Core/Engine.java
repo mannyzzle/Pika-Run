c9db5f58356ab1888cec66d3569e2791e1d4c4b8
Enmanuel Hernandez <112276581+mannyzzle@users.noreply.github.com>
2024-05-02 22:13:22 -0700

    Update Engine.java

--- Core/Engine.java
+++ Core/Engine.java
@@ -97,15 +97,11 @@ public class Engine {
     }
 
     /**
-     * Method used for autograding and testing your code. The input string will be a series
+     * Method used for  testing the code. The input string will be a series
      * of characters (for example, "n123sswwdasdassadwas", "n123sss:q", "lwww". The engine should
      * behave exactly as if the user typed these characters into the engine using
      * interactWithKeyboard.
      *
-     * Recall that strings ending in ":q" should cause the game to quite save. For example,
-     * if we do interactWithInputString("n123sss:q"), we expect the game to run the first
-     * 7 commands (n123sss) and then quit and save. If we then do
-     * interactWithInputString("l"), we should be back in the exact same state.
      *
      * In other words, both of these calls:
      *   - interactWithInputString("n123sss:q")
@@ -122,8 +118,7 @@ public class Engine {
         // world that would have been drawn if the same inputs had been given
         // to interactWithKeyboard().
         //
-        // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
-        // that works for many different input types.
+        
         iSTRING = true;
         if (remainingInput.charAt(0) == 'l') {
             String saved = readContentsAsString(SAVED);
